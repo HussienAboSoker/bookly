95dea7a5ab9e83292021a0e2e4639ecc2413ce4c
HussienAboSoker <128934263+HussienAboSoker@users.noreply.github.com>
2024-04-15 13:58:28 +0200

    use mvvm patren in project and add get

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const Bookly());
@@ -10,6 +11,6 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const GetMaterialApp();
   }
 }
