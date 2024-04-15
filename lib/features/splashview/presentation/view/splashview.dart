aaa005df5c3b50e940751a414826e3b41f692d5d
HussienAboSoker <128934263+HussienAboSoker@users.noreply.github.com>
2024-04-15 14:21:30 +0200

    cahnge name

--- lib/features/splashview/presentation/view/splashview.dart
+++ lib/features/splashview/presentation/view/splashview.dart
@@ -1,4 +1,4 @@
-import 'package:bookly/features/splashview/presentation/view/widget/SplashViewbody.dart';
+import 'package:bookly/features/splashview/presentation/view/widget/splashviewbody.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
